1b4401b8fcb327a677ae26a49983d157cd254ea3
SivasaravanarajanS <yuvansrvnn@gmail.com>
2017-06-20 15:02:42 +0530

    Update evenorodd.java

--- evenorodd.java
+++ evenorodd.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
-import java.util.Scanner;
 class evenorodd {
     public static void main(String[] args) 
     {
