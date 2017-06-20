209bd729d4a3f187a3812a8a4afaa98a40666f82
SivasaravanarajanS <yuvansrvnn@gmail.com>
2017-06-20 15:00:56 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Scanner;
+class evenorodd {
+    public static void main(String[] args) 
+    {
+Scanner s=new Scanner(System.in);
+System.out.println("ENTER THE VALUE");
+        int number=s.nextInt();
+        if(number%2==0)
+            System.out.println("ENTERED VALUE IS EVEN");
+                else
+            System.out.println("ENTERED VALUE IS ODD");
+}
+        
+    }
