1de6ed59abd79943eb4e2da61ba733c719d77cf4
SivasaravanarajanS <yuvansrvnn@gmail.com>
2017-06-20 17:13:49 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+ class hello {
+   public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+          String new1="Hello";
+      for(int i=0;i<5;i++)
+      {
+      
+          System.out.println(new1);
+    }
+    }
+}
+    
+
