8c63f4c45785bba5b28c3a984143b408cc5816ff
SivasaravanarajanS <yuvansrvnn@gmail.com>
2017-06-22 12:25:06 +0530

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+ class stringtoint{
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String new1=s.nextLine();
+
+int i = Integer.parseInt(new1);
+System.out.println(i);
+}
+}
