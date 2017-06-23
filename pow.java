6c5e9695736f0dfe222d984c1b7580a1e9cb3ed2
SivasaravanarajanS <yuvansrvnn@gmail.com>
2017-06-23 14:42:56 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,15 @@
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class pow
+{
+	public static void main (String[] args){
+ Scanner s=new Scanner(System.in);
+ int a=s.nextInt();
+ int b;
+ b=(int) pow(a,3);
+ System.out.println(b);
+ }
+ }
